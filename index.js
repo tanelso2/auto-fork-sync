1a7fd1dcf510041e09f77f3d866c5e4eb63bcac2
Thomas Nelson <tanelso2@gmail.com>
2018-02-01 20:09:44 -0800

    Got it working so that pushes to the parent syncs to all the children
    Caveats:
        - The branch must already exist on the child
        - It does not handle adding or deleting branches yet
        - It sends a ton of emails and GitHub notifications
        - It has no error handling

--- index.js
+++ index.js
@@ -1,10 +1,73 @@
 module.exports = (robot) => {
   // Your code here
   robot.log('Yay, the app was loaded!')
+  robot.log(robot)
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  robot.on('push', async context => {
+    robot.log(context)
+    const github = context.github
+    const payload = context.payload
+    const forks = await getListOfForks(payload.repository, github)
+    const branchName = payload.ref.substring(11) // Gets rid of the refs/head part
+    const parentRepo = getRepoDict(payload.repository)
+    for (const fork of forks) {
+      await updateChildBranch(robot, branchName, parentRepo, fork)
+    }
+  })
+}
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+function getRepoDict (repository) {
+  return {
+    owner: repository.owner.login,
+    repo: repository.name
+  }
+}
+
+async function getListOfForks (repository, github) {
+  const {owner, repo} = getRepoDict(repository)
+  const result = await github.repos.getForks({owner, repo})
+  const forks = result.data.map(getRepoDict)
+  return forks
+}
+
+async function updateChildBranch (robot, branchName, parentRepo, childRepo) {
+  const appClient = await robot.auth()
+  const github = await getClientForRepo(robot, appClient, childRepo)
+  const payload = {
+    title: `[Auto Fork Sync] Updating branch ${branchName}`,
+    owner: childRepo.owner,
+    repo: childRepo.repo,
+    head: `${parentRepo.owner}:${branchName}`,
+    base: branchName,
+    body: 'Auto Fork Sync engaged',
+    maintainer_can_modify: false
+  }
+  robot.log(`Trying to create a pullRequest on child ${childRepo.owner}:${childRepo.repo}`)
+  const result = await github.pullRequests.create(payload)
+  const pullRequestId = result.data.number
+  console.log(`Attempting to merge pull request ${pullRequestId} on ${childRepo.owner}/${childRepo.repo}`)
+  const mergeResult = await github.pullRequests.merge({
+    owner: childRepo.owner,
+    repo: childRepo.repo,
+    number: pullRequestId,
+    merge_method: 'rebase'
+  })
+  console.log(mergeResult)
+  if (mergeResult.data.merged) {
+    console.log('Hooray! The merge worked!')
+  }
+}
+
+async function getClientForRepo (robot, appClient, {owner}) {
+  // TODO: Paginate
+  // TODO: Cache
+  const installations = await appClient.apps.getInstallations({})
+  robot.log('INSTALLATIONS')
+  robot.log(installations)
+  for (const installation of installations.data) {
+    if (installation.account.login === owner) {
+      return robot.auth(installation.id)
+    }
+  }
+  return undefined // Should figure out how error handling works.
 }
